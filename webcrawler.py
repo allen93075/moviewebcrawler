57158a4a981b22dd1d0182c82d6634ae9d5c4800
allen93075 <50901621+allen93075@users.noreply.github.com>
2021-03-25 15:20:54 +0800

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-import json
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
+import os
+import starter as st
+import time
 
 # Lux cinema
 
@@ -45,7 +46,7 @@ def get_href(soup):
 
 
 # 拿到月、日、場次時間
-def get_time(url="https://www.luxcinema.com.tw/web/2020-movie_item.php?film_id=1860"):
+def get_time(url="https://www.luxcinema.com.tw/web/2020-movie_item.php?film_id=1879"):
     day = []
     date_and_time = []
     r = requests.get(url)
@@ -55,21 +56,15 @@ def get_time(url="https://www.luxcinema.com.tw/web/2020-movie_item.php?film_id=1
     for zz in daylist:
         day.append(month + "/" + zz.text)
 
-    time_list = s.find_all("div", {"class": "time_list"}, limit=2)
-    time1 = time_list[0].find_all("b")
-    time2 = []
-    if len(time_list) > 1:
-        time2 = time_list[1].find_all("b")
+    # 找出所有場次的時間和日期
+    time_list = s.find_all("div", {"class": "time_list"})
+    for i in range(len(time_list)):
+        time_for_link = []
+        for j in time_list[i].find_all("b"):
+            time_for_link.append(j.text[:5])
 
-    for x in time1:
-        check = x.text
-        check = check[:5]
-        time2.append(check)
-        # print(time2)
-    for y in time2:
-        date_and_time.append(day[0] + " " + y)
-
-
+        for k in time_for_link:  # 將日期和時間傳出成為一個list
+            date_and_time.append(day[i] + " " + k)
 
     # Link for get_seat_count
     link = s.find_all("div", {"class": "time_list"})
@@ -79,31 +74,28 @@ def get_time(url="https://www.luxcinema.com.tw/web/2020-movie_item.php?film_id=1
         for k in z:
             urlforseat.append("https://www.luxcinema.com.tw/web/" + k.get('href'))
 
-    print(date_and_time)
-    print(urlforseat)
-
     return date_and_time, urlforseat
 
 
 def get_link(url="https://www.luxcinema.com.tw/web/2020-movie_item.php?film_id=1850"):
     r = requests.get(url)
     s = BeautifulSoup(r.text, "html.parser")
-    print(s)
+    # print(s)
 
 
-def get_seat_count(url):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+def get_seat_count(url='https://www.luxcinema.com.tw/web/2020_sel_ticket.php?serial_number=dfe884434a156c03f92d887873b6b370&film_id=1879&showdate=0&showtime=2021-03-24&sel_ticket_id=129586&seat_type=SEAT'):
+    driver = st.start_drive()
     driver.get(url)
-    driver.execute_script("ticket_Plus(326,2)")
+    driver.execute_script("ticket_Plus(324,2)")
+    driver.execute_script("ticket_Plus(41,2)")
     driver.execute_script("ticket_function()")
     b = driver.page_source
     s = BeautifulSoup(b, "html.parser")
     resu = s.find_all("img", {"src": "images/lux_btn/lux_seat_01.png"})
-    print(len(resu))
+    # print(len(resu))
     driver.close()
 
     return len(resu)
-
 
 # get_name > get_href >
 # get_time()
@@ -112,24 +104,23 @@ def excute_webcrawler():
     name = get_name(result)
     url = get_href(soup)
     webresult = []
-    for i in range(len(name)):
-        try:
+    try:
+        for i in range(len(name)):
             dic_1 = {'Name': name[i]}
             time, seat_url = get_time(url[i])
-            print(time)
+            # print(dic_1)
+            # print(time)
             for index in range(len(time)):
                 dic_1['Time'] = time[index]
                 seat = get_seat_count(seat_url[index])
+                print(seat)
                 dic_1['seat'] = seat
+                dic_1['theater'] = "樂聲影城"
+                print(dic_1)
                 webresult.append(dic_1.copy())
-                print(webresult)
+                # print(webresult)
+    except:
+        pass
 
-        except:
-            pass
-
-    print(webresult)
+    
     return webresult
-
-
-
-excute_webcrawler()
